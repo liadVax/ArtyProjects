68fb40a8449f3ac90e28b0ad6773f109157230c1
liadVax <61513291+liadVax@users.noreply.github.com>
2020-09-05 14:04:19 +0300

    Update 10Print.js

--- 10Print.js
+++ 10Print.js
@@ -2,7 +2,7 @@ let x = 0;
 let y = 0;
 let offset = 20;
 let probe;
-let color = ["#2a9d8f", "#e9c46a", "#f4a261", "#e76f51"];
+let colors = ["#2a9d8f", "#e9c46a", "#f4a261", "#e76f51"];
 let quarter = 1;
 let flag = 0;
 let cnt = 0;
@@ -37,7 +37,7 @@ function draw() {
   probe = random(1);
   strokeWeight(6);
   if (sel.value() == "Random") {
-    stroke(color[floor(random(4))]);
+    stroke(colors[floor(random(4))]);
     if (probe < 0.4) {
       line(x, y, offset + x, y + offset); //-  \
     } else if (probe > 0.4 && probe < 0.9) {
@@ -51,14 +51,14 @@ function draw() {
     if (x >= width / 2 && y <= height / 2) quarter = 2;
     if (x <= width / 2 && y >= height / 2) quarter = 3;
     if (x >= width / 2 && y >= height / 2) quarter = 4;
-    stroke(color[quarter - 1]);
+    stroke(colors[quarter - 1]);
     if (quarter == 1 || quarter == 4) {
       line(x, y + offset, x + offset, y); //- /
     } else if (quarter == 2 || quarter == 3) {
       line(x, y, offset + x, y + offset); //-  \
     }
   } else if (sel.value() == "Zigzag") {
-    stroke(color[cnt]);
+    stroke(colors[cnt]);
     if (flag) {
       line(x, y, offset + x, y + offset);
     } else {
